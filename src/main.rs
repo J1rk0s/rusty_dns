e0a84eb4742a429fed934091c96d02d93924c3d4
Kris <jiri.vimmer123@seznam.cz>
2025-01-17 21:52:11 +0100

    Added tests

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-use server::DnsServer;
-
-mod server;
-mod models;
+use rusty_dns::server::DnsServer;
 
 fn main() {
     let dns = DnsServer::init("192.168.1.50", 53);
---
+++ tests/packet_tests.rs
@@ -0,0 +1,17 @@
+#[cfg(test)]
+mod tests {
+    use rusty_dns::models::structure::DnsPacket;
+
+    #[test]
+    fn test_parsing(){
+        let data: [u8; 33] = [0xca, 0x38, 0x1, 0x0, 0x0, 0x1, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x3, 0x77, 0x77, 0x77, 0x7, 0x79, 0x6f, 0x75, 0x74, 0x75, 0x62, 0x65, 0x3, 0x63, 0x6f, 0x6d, 0x0, 0x0, 0x1, 0x0, 0x1];
+        let packet = DnsPacket::parse(&data);
+
+        assert_eq!(packet.header.id, 51768);
+        assert_eq!(packet.header.flags, 256);
+        assert_eq!(packet.header.qdcount, 1);
+        assert_eq!(packet.header.ancount, 0);
+        assert_eq!(packet.header.nscount, 0);
+        assert_eq!(packet.header.arcount, 0);
+    }
+}
