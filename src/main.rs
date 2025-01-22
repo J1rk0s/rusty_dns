5687c58ab8a5fbb862b3f7ac684c06c44863f874
Kris <jiri.vimmer123@seznam.cz>
2025-01-22 17:09:22 +0100

    Added clap for cli parsing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
 use rusty_dns::server::DnsServer;
 use std::io::Result;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    ip: String,
+
+    #[arg(short, long, default_value_t = 53)]
+    port: u16
+}
 
 fn main() -> Result<()> {
-    let dns = DnsServer::init("192.168.1.50", 53);
+    let args = Args::parse();
+
+    let dns = DnsServer::init(&args.ip, args.port);
     dns.run()
 }
